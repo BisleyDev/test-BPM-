2765f07fd7424aeb4262079b0546ddb2b70b61ba
BisleyDev <andrey.bisley@yahoo.com>
2020-12-21 02:04:04 +0200

    refactoring

--- testData.js
+++ testData.js
@@ -1,41 +1,9 @@
-//   Test Objects Template
-// const MAIN_DATA = [
-// 	{ fullName: { surname: 'xxx', firstName: 'yyy', middleName: 'zzz' } },
-// 	{ fullName: { surname: 'XXX', firstName: 'YYY', middleName: 'ZZZ' } },
-// ];
-// const RULE_DATA = {
-// 	fullName: { surname: true, firstName: true, middleName: false },
-// };
-
-//  My Objects Template
 const MAIN_DATA = [
 	{ fullName: { surname: 'xxx', firstName: 'yyy', middleName: 'zzz' } },
-	{
-		fullName: {
-			surname: 'XXX',
-			firstName: 'YYY',
-			middleName: 'ZZZ',
-			parents: {
-				date: new Date(),
-				mother: { name: 'Anna', age: '45' },
-				father: { name: 'Igor', age: 42 },
-			},
-		},
-	},
+	{ fullName: { surname: 'XXX', firstName: 'YYY', middleName: 'ZZZ' } },
 ];
-
 const RULE_DATA = {
-	fullName: {
-		surname: true,
-		firstName: true,
-		middleName: false,
-		parents: {
-			date: true,
-			mother: { name: true, age: false },
-			father: { name: false, age: true },
-		},
-		dateStart: true,
-	},
+	fullName: { surname: true, firstName: true, middleName: false },
 };
 
 const LOCAL_DATA = {
@@ -43,54 +11,22 @@ const LOCAL_DATA = {
 	'fullName.middleName': 'По-батькові',
 };
 
-function getNewArrayData(mainData, ruleData, LocalData) {
-	const arrayRules = createPathRules(ruleData);
-	let newArray = createList(mainData, arrayRules, LocalData);
-	return newArray;
+function getNewArrayData(mainData, ruleData, localData) {
+	return createList(mainData, createPathRules(ruleData), localData);
 }
 
 function createList(data, arrayRules, localData) {
-	const arrayRule = arrayRules.map((el) => el.replace('.', ''));
 	const newArrayData = [];
 
-	for (rule of arrayRule) {
+	for (let rule of arrayRules) {
 		const newObjRule = {};
-
-		newObjRule.name = createValueName(rule, localData);
-
-		function createValueName(rule, location) {
-			const value = Object.keys(location).find((el) => el === rule);
-			if (value) {
-				return location[value];
-			} else {
-				const array = rule.split('.');
-				return array[array.length - 1];
-			}
-		}
+		newObjRule.name = localData[rule] || rule.split('.').pop();
 
 		for (let i = 0; i < data.length; i++) {
-			const obj = data[i];
-			let path = rule.split('.');
+			const value = findValueInObj(data[i], rule.split('.'));
 
-			function findValueInObj(obj, arrayKey, index = 0) {
-				const key = arrayKey[index];
-				const checkValue = obj[key];
-
-				if (!arrayKey[index]) {
-					return;
-				} else if (checkValue instanceof Date) {
-					newObjRule[`value${i + 1}`] = getDate(checkValue.getTime());
-					return;
-				} else if (typeof checkValue === 'object') {
-					return findValueInObj(checkValue, arrayKey, ++index);
-				} else if (checkValue) {
-					newObjRule[`value${i + 1}`] = checkValue;
-					return;
-				}
-			}
-			findValueInObj(obj, path);
+			value ? (newObjRule[`value${i + 1}`] = value) : null;
 		}
-
 		newArrayData.push(newObjRule);
 	}
 	return newArrayData;
@@ -101,29 +37,51 @@ function createPathRules(data) {
 	function recursive(data, path) {
 		for (let key in data) {
 			if (typeof data[key] === 'object') {
-				return recursive(data[key], (path += `.${key}`));
+				recursive(data[key], [path, key].filter(Boolean).join('.'));
+			} else {
+				data[key]
+					? arrayRules.push([path, key].filter(Boolean).join('.'))
+					: null;
 			}
-			data[key] ? arrayRules.push(path + '.' + key) : null;
 		}
 	}
 	recursive(data, '');
 	return arrayRules;
 }
 
+function findValueInObj(obj, arrayKey, index = 0) {
+	const key = arrayKey[index];
+	const checkValue = obj[key];
+
+	if (!arrayKey[index]) {
+		return;
+	} else if (typeof checkValue === 'boolean') {
+		return checkValue ? 'Так' : 'Ні';
+	} else if (checkValue instanceof Date) {
+		return getDate(checkValue.getTime());
+	} else if (
+		typeof checkValue === 'number' ||
+		typeof checkValue === 'string'
+	) {
+		return checkValue;
+	} else if (typeof checkValue === 'object') {
+		return findValueInObj(checkValue, arrayKey, ++index);
+	}
+}
+
 function getDate(dateNumber) {
 	const date = new Date(dateNumber);
-
-	const day = checkNumber(date.getDate());
-	const month = checkNumber(date.getMonth(date) + 1);
-	const year = date.getFullYear(date);
-
 	function checkNumber(number) {
 		if (number < 10) {
 			return '0' + number;
 		}
 		return number;
 	}
-	return `${day}.${month}.${year}`;
+	return [
+		checkNumber(date.getDate()),
+		checkNumber(date.getMonth() + 1),
+		date.getFullYear(),
+	].join('.');
 }
 
 const needArray = getNewArrayData(MAIN_DATA, RULE_DATA, LOCAL_DATA);
